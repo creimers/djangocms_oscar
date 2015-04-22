a22192aa4461e0a02bd89084fbe2784c84192c3a
Christoph Reimers <christoph@superservice-international.com>
2015-04-22 09:12:51 +0200

    replaced __unicode__ with python3 comp. __str__ method

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from cms.models import CMSPlugin
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class FeaturedProduct(CMSPlugin):
     product = models.ForeignKey('catalogue.Product')
 
-    def __unicode__(self):
+    def __str__(self):
         if self.product:
             return self.product.get_title()
         return ''

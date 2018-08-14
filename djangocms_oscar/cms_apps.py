791ce33e120747c09915773e429d77ccea0b3f55
Christoph Reimers <christoph@superservice-international.com>
2018-08-14 17:07:06 +0200

    prepare urls for django > 1.9

--- djangocms_oscar/cms_apps.py
+++ djangocms_oscar/cms_apps.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.utils.translation import ugettext_lazy as _
-from django.conf.urls import patterns
 from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
 from oscar.app import application
@@ -13,8 +12,9 @@ class OscarApp(CMSApp):
     """
     name = _("Oscar")
     urls = [
-        patterns('', *application.urls[0])
+        *application.urls[0]
     ]
     exclude_permissions = ['dashboard']
+
 
 apphook_pool.register(OscarApp)
